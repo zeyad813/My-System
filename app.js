1b9a15bba69dd15e15f2a87ce4380225b4582525
zeyad813 <136343265+zeyad813@users.noreply.github.com>
2026-03-24 17:08:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 
+const tabs = ["Habits","Meals","ToDo","Review","Identity"];
+
+const pagesEl = document.getElementById("pages");
+const tabsEl = document.getElementById("tabs");
+
+let state = {
+  habits: JSON.parse(localStorage.getItem("habits")||"[]"),
+  todos
